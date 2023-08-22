d8edb912b745d8d1fb644739a1838793ae08b091
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-08-22 10:59:00 +0100

    add ReactivceForms

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,23 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../task.service';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  form?: FormGroup;
 
   constructor(private taskService: TaskService) {}
 
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(null,{
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      description: new FormControl(null,{
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(180)]
+      }),
+      priority: new FormControl(null,{
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.min(1), Validators.max(5)],
+      }),
+      startDate: new FormControl(null,{
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      endDate: new FormControl(null,{
+        updateOn: 'blur',
+        validators: [Validators.required]
+      })
+    })
+  }
+
+
+
   ionViewWillEnter(){
-    this.taskService.saveTask({
-      title: 'task title',
-      description: 'task description',
-      startDate: new Date(),
-      endDate: new Date(),
-      priority: 10
-    }).subscribe()
+
+  }
+
+  onCreateTask(){
+    console.log(this.form)
   }
 
 }
